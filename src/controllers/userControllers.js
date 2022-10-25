9c3405e8cc54ea3eaeeea35e1a12f2094fa1a230
mae916 <ruchia916@naver.com>
2022-10-25 22:36:03 +0900

    github 로그인 완료

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -28,10 +28,10 @@ export const postJoin = async (req, res) => {
   }
   try {
     await User.create({
-      email,
-      username,
-      password,
       name,
+      username,
+      email,
+      password,
       location,
     });
   } catch (error) {
@@ -51,7 +51,7 @@ export const getLogin = (req, res) => {
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
 
   // username 확인
   if (!user) {
@@ -71,7 +71,6 @@ export const postLogin = async (req, res) => {
   }
   req.session.loggedIn = true; // 로그인 완료
   req.session.user = user; // 유저가 로그인 하면 그 유저에 대한 정보를 세션에 담기
-
   return res.redirect("/");
 };
 
@@ -94,6 +93,7 @@ export const finishGithubLogin = async (req, res) => {
     client_secret: process.env.GH_SECRET,
     code: req.query.code,
   };
+
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   const tokenRequest = await (
@@ -107,15 +107,50 @@ export const finishGithubLogin = async (req, res) => {
   if ("access_token" in tokenRequest) {
     //변수 in 객체
     const { access_token } = tokenRequest; //json에서 access_token를 뽑아오고,
-    const userRequest = await (
-      await fetch("https://api.github.com/user", {
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
         //토큰으로 API 접근해서
         headers: {
           Authorization: `Bearer ${access_token}`,
         },
       })
     ).json(); //json 형식으로 user 정보 가져오기
-    console.log(userRequest);
+    console.log(userData);
+
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(emailData);
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      //알림
+      return res.redirect("/login");
+    }
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      // db에 저장된 email과 github에서 받아온 email이 같지 않은 유저가 없으면
+      //github에서 받아온 user 정보로 계정 생성
+      user = await User.create({
+        name: userData.name ? userData.name : userData.login, // name:null인 경우가 있음
+        avatarUrl: userData.avatar_url,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+    }
+    //같은 email이 있는 경우 로그인 시킴
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
@@ -125,12 +160,9 @@ export const edit = (req, res) => {
   res.send("Edit User");
 };
 
-export const remove = (req, res) => {
-  res.send("Delete User");
-};
-
 export const logout = (req, res) => {
-  res.send("LOGOUT");
+  req.session.destroy();
+  return res.redirect("/");
 };
 
 export const see = (req, res) => {
--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false }, //github로 로그인 했는지 여부를 알기 위해서 추가
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
 });
